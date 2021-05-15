c13e1a9d287965b87db35c30d7436a9ad66136dd
Kajal764 <57980205+Kajal764@users.noreply.github.com>
2021-05-15 20:55:19 +0530

    class
    commit mesage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import "./App.css";
 import TitleName from '../src/titlename'
 import Main from '../src/main'
-
+//class
 export class App extends Component {
   render() {
     return (
